68c6e2ffd29d7b54e5321f6f2c3fddcbfff326be
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2017-06-30 13:15:39 -0700

    Allow stateless functional components in `connect`.

--- packages/react-redux-modules/src/connectComponent.js
+++ packages/react-redux-modules/src/connectComponent.js
@@ -3,22 +3,30 @@ import {map} from 'lodash';
 import PropTypes from 'prop-types';
 import {Component, createElement} from 'react';
 import {bindActionCreators, type Dispatch} from 'redux';
-import {connect as reactReduxConnect, type MapStateToProps} from 'react-redux';
+import {
+  connect as reactReduxConnect,
+  type MapStateToProps,
+  type StatelessComponent,
+} from 'react-redux';
 
 import type {Action, ReduxModule} from '@wtg/redux-modules';
 
 export default function connectComponent<S: Object, OP: Object, SP: Object>(
   selector: MapStateToProps<S, OP, SP>,
-  modules: Array<ReduxModule<*, *, *>>,
+  modules: Array<ReduxModule<Object, any, any>>,
 ) {
   const actionCreators = Object.assign({}, ...map(modules, 'actionCreators'));
-  const mapDispatchToProps = (dispatch: Dispatch<Action<*, *>>) =>
+  type DP = typeof actionCreators;
+  type P = $Supertype<OP & SP & DP>;
+  const mapDispatchToProps = (dispatch: Dispatch<Action<any, any>>) =>
     bindActionCreators(actionCreators, dispatch);
   const reactReduxConnectComponent = reactReduxConnect(
     selector,
     mapDispatchToProps,
   );
-  return (NestedComponent: Class<React$Component<void, OP, void>>) => {
+  return <Def, St>(
+    component: StatelessComponent<P> | Class<React$Component<Def, P, St>>,
+  ) => {
     class Connect extends Component {
       static contextTypes = {
         registerModules: PropTypes.func,
@@ -32,7 +40,7 @@ export default function connectComponent<S: Object, OP: Object, SP: Object>(
       }
 
       render() {
-        return createElement(NestedComponent, this.props);
+        return createElement(component, this.props);
       }
     }
 
